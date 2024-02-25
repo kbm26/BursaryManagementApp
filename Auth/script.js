903eb25cdc0af132d4b65aabec1695b29341494e
kylebmoodley <kylebmoodley@gmail.com>
2024-02-25 19:45:43 +0200

    authentication added in html only

---
+++ Auth/script.js
@@ -0,0 +1,6 @@
+var params = {'client_id': '701328728829-c507ja8sc0q410m1bg250l7spinh0g5m.apps.googleusercontent.com',
+'redirect_uri': 'http://localhost:5500',
+'response_type': 'token',
+'scope': ' https://www.googleapis.com/auth/userinfo.profile',
+'include_granted_scopes': 'true',
+'state': 'pass-through value'};
