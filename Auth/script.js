a34fafbdb3ba89c4df147b11f1901847ca71e7ee
kylebmoodley <kylebmoodley@gmail.com>
2024-02-26 13:23:54 +0200

    auth redirect done

--- Auth/script.js
+++ Auth/script.js
@@ -1,6 +1,105 @@
-var params = {'client_id': '701328728829-c507ja8sc0q410m1bg250l7spinh0g5m.apps.googleusercontent.com',
-'redirect_uri': 'http://localhost:5500',
-'response_type': 'token',
-'scope': ' https://www.googleapis.com/auth/userinfo.profile',
-'include_granted_scopes': 'true',
-'state': 'pass-through value'};
+const btn = document.querySelector('.button')
+
+const YOUR_CLIENT_ID = '701328728829-c507ja8sc0q410m1bg250l7spinh0g5m.apps.googleusercontent.com';
+const YOUR_REDIRECT_URI = 'http://localhost:5500';
+let fragmentString = location.hash.substring(1);
+let email =""
+let params = {};
+let regex = /([^&=]+)=([^&]*)/g, m;
+
+while (m = regex.exec(fragmentString)) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+}
+
+if (Object.keys(params).length > 0) {
+    localStorage.setItem('oauth2-test-params', JSON.stringify(params) );
+    if (params['state'] && params['state'] == 'try_sample_request') {
+        trySampleRequest();
+    }
+}
+
+function trySampleRequest() {
+    let params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+    if (params && params['access_token']) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET',
+            'https://www.googleapis.com/oauth2/v3/userinfo?' +
+            'access_token=' + params['access_token']);
+        xhr.onreadystatechange = function (e) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            email = JSON.parse(xhr.response)["email"]
+            verifyrole()
+        } else if (xhr.readyState === 4 && xhr.status === 401) {
+            oauth2SignIn();
+        }
+        };
+        xhr.send(null);
+    } else {
+        oauth2SignIn();
+    }
+}
+
+
+function oauth2SignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    let oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    // Create element to open OAuth 2.0 endpoint in new window.
+    let form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    let params = {'client_id': YOUR_CLIENT_ID,
+                    'redirect_uri': YOUR_REDIRECT_URI,
+                    'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                    'state': 'try_sample_request',
+                    'include_granted_scopes': 'true',
+                    'response_type': 'token'};
+
+    // Add form parameters as hidden input values.
+    for (let p in params) {
+        let input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit()
+}
+
+async function verifyrole(){
+
+    try{
+    const response = await fetch("https://localhost:7162/api/Login/byEmail?userEmail="+email)
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    roleFinder(data["roleID"])
+    } catch (error) {
+    // Handle errors
+    console.error('There was a problem with your fetch operation:', error);
+}
+
+/**
+ * 
+ * @param {int} role 
+ * @returns {string} roleInfo
+ */
+function roleFinder(role ){
+    return (role === 1) ? window.location.href = "./BBD.html" : (role === 2) ? window.location.href = "./HOD.html"  : "None";
+}
+    
+
+}
+
+btn.addEventListener("click", trySampleRequest);
+
+
+
+
