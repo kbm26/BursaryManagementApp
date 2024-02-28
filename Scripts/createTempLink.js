e446c735446100c7c7ddbe2d74239c0091676428
Jordan Beaumont <jordanbmnt@gmail.com>
2024-02-28 16:39:51 +0200

    added functionality to go to create temporary link

--- Scripts/createTempLink.js
+++ Scripts/createTempLink.js
@@ -2,15 +2,7 @@ const tempLink = document.getElementById("createTempLink");
 const assignReviewer = document.getElementById("assignReviewer");
 
 assignReviewer.addEventListener("click", () => {
-  if (tempLink.style.display === "none") {
-    tempLink.style.display = "flex";
-    setTimeout(() => {
-      tempLink.style.opacity = 1;
-    }, 200);
-  } else {
-    tempLink.style.opacity = 0;
-    setTimeout(() => {
-      tempLink.style.display = "none";
-    }, 600);
-  }
+  window.location.pathname = `/${
+    window.location.pathname.split("/")[1]
+  }/studenttemporarylink`;
 });
