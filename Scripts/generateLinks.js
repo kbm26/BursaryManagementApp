c95999efb0bde72481c873638c76c5a139e62910
Ryshan172 <ryshanramlall@outlook.com>
2024-02-28 21:27:33 +0200

    Completed Connecting Generate Links to frontend

--- Scripts/generateLinks.js
+++ Scripts/generateLinks.js
@@ -1,19 +1,19 @@
 
-// Generate a Temporary Link for a student (Need to provide StudentIDNum)
-document.getElementById('generateTokenForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+ // Generate a Temporary Link for a student (Need to provide StudentIDNum)
+ document.getElementById("tempLinkForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent page reload
 
     // Get studentIDNum input value
-    const studentIDNum = document.getElementById('studentIDNum').value;
+    const studentIDNum = document.getElementById("studentIDNum").value;
 
     // Endpoint URL with the studentIDNum parameter
     const url = `https://bursarywebapp.azurewebsites.net/api/Token/generateToken?studentIDNum=${studentIDNum}`;
 
     // Define request options
     const requestOptions = {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'accept': '*/*'
+            "accept": "*/*"
         }
     };
 
@@ -21,20 +21,20 @@ document.getElementById('generateTokenForm').addEventListener('submit', function
     fetch(url, requestOptions)
         .then(response => {
             if (response.ok) {
-                // Extract token URL from the response
+                // Get token URL 
+                console.log(response.json);
                 return response.json();
             } else {
-                // Display error message
-                document.getElementById('tokenUrl').textContent = 'Error: ' + response.statusText;
+                document.getElementById("tokenUrl").textContent = "Error: " + response.statusText;
             }
         })
         .then(data => {
-            // Display token URL
-            document.getElementById('tokenUrl').textContent = 'Token URL: ' + data.tokenUrl;
+            // Display token URL to copy
+            document.getElementById("tokenUrl").textContent = "Link generated: " + data.tokenUrl;
         })
         .catch(error => {
-            // Handle errors
-            console.error('Error:', error);
-            document.getElementById('tokenUrl').textContent = 'Error: ' + error.message;
+
+            console.error("Error:", error);
+            document.getElementById("tokenUrl").textContent = "Error: " + error.message;
         });
 });
