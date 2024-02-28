68fa152c51deba30c07acca5a6bc438556858b8c
kylebmoodley <kylebmoodley@gmail.com>
2024-02-28 10:30:19 +0200

    see all universities

--- Scripts/universityCollections.js
+++ Scripts/universityCollections.js
@@ -1,208 +1,19 @@
-const unis = [
-  {
-    applicationID: 1,
-    status: "pending",
-    amountRequested: 360000,
-    name: "University of KwaZulu-Natal",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 2,
-    status: "approved",
-    amountRequested: 288000,
-    name: "one",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 3,
-    status: "rejected",
-    amountRequested: 240000,
-    name: "runs",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 4,
-    status: "pending",
-    amountRequested: 192000,
-    name: "UKZN",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 5,
-    status: "pending",
-    amountRequested: 192000,
-    name: "UKZN",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 6,
-    status: "pending",
-    amountRequested: 240000,
-    name: "one",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 7,
-    status: "pending",
-    amountRequested: 168000,
-    name: "runs",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 8,
-    status: "pending",
-    amountRequested: 360000,
-    name: "UKZN",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 9,
-    status: "pending",
-    amountRequested: 192000,
-    name: "UKZN",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 10,
-    status: "pending",
-    amountRequested: 168000,
-    name: "one",
-    applicationYear: 2022,
-    isLocked: true,
-  },
-  {
-    applicationID: 11,
-    status: "pending",
-    amountRequested: 180000,
-    name: "University of KwaZulu-Natal",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 12,
-    status: "pending",
-    amountRequested: 144000,
-    name: "one",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 13,
-    status: "pending",
-    amountRequested: 120000,
-    name: "runs",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 14,
-    status: "pending",
-    amountRequested: 96000,
-    name: "UKZN",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 15,
-    status: "pending",
-    amountRequested: 96000,
-    name: "UKZN",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 16,
-    status: "pending",
-    amountRequested: 120000,
-    name: "one",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 17,
-    status: "pending",
-    amountRequested: 84000,
-    name: "runs",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 18,
-    status: "pending",
-    amountRequested: 180000,
-    name: "UKZN",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 19,
-    status: "pending",
-    amountRequested: 96000,
-    name: "UKZN",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-  {
-    applicationID: 20,
-    status: "pending",
-    amountRequested: 84000,
-    name: "one",
-    applicationYear: 2023,
-    isLocked: true,
-  },
-];
-
 const table = document.getElementById("dataTable");
-
-unis.forEach((uni, i) => {
-  rowAdder(table, i, uni);
-});
-
-statusColorCoder();
-
-const userDataInserter = ({ name, element, data }) => {
-  element.innerHTML = ` <form action="">
-      <h1>${name}(${data.applicationYear})</h1>
-      <section class="formInput">
-        <label for="status">Application Status:</label>
-        <select disabled name="Status" id="status">
-        <option ${
-          data.status == "pending" && "selected"
-        } value="1">Pending</option>
-        <option ${
-          data.status == "approved" && "selected"
-        } value="2">Approved</option>
-        <option ${
-          data.status == "rejected" && "selected"
-        } value="3">Rejected</option>
-      </select>
-      </section>
-      <section class="formInput">
-        <label for="name">Amount Requested:</label>
-        <input class="userData" disabled placeholder=${
-          data.amountRequested
-        }  type="number" name="name">
-      </section>
-      <section class="dataModButtons">
-      <button class="deleteData" type="submit">Delete</button>
-      <button class="updateData" type="submit">Update</button>
-      </section>
-    </form>`;
-};
-
 const viewUniversityButtons = document.getElementsByClassName("viewUniversity");
+
+let unis
+
+function tableMaker(list){
+  list.forEach((uni, i) => {
+    rowAdder(table, i, {name: uni.universityName, status: uni.applicationStatusID === 1 ?
+       "pending" : uni.applicationStatusID === 2?
+      "approved" : "rejected" });
+  });
+}
 
 const redirectToUniInfo = (e) => {
   const tableRow = e.target.parentNode.parentNode;
+  console.log(tableRow)
   const uniName = tableRow.childNodes[0].textContent;
   const rowPos = parseInt(tableRow.id) + 2;
 
@@ -236,5 +47,70 @@ const redirectToUniInfo = (e) => {
     }, 860);
   }
 };
-for (const b of viewUniversityButtons)
-  b.addEventListener("click", redirectToUniInfo);
+
+const userDataInserter = ({ name, element, data }) => {
+  element.innerHTML = ` <form action="">
+      <h1>${name}(${data.applicationYear})</h1>
+      <section class="formInput">
+        <label for="status">Application Status:</label>
+        <select disabled name="Status" id="status">
+        <option ${
+          data.status == "pending" && "selected"
+        } value="1">Pending</option>
+        <option ${
+          data.status == "approved" && "selected"
+        } value="2">Approved</option>
+        <option ${
+          data.status == "rejected" && "selected"
+        } value="3">Rejected</option>
+      </select>
+      </section>
+      <section class="formInput">
+        <label for="name">Amount Requested:</label>
+        <input class="userData" disabled placeholder=${
+          data.amountRequested
+        }  type="number" name="name">
+      </section>
+      <section class="dataModButtons">
+      <button class="deleteData" type="submit">Delete</button>
+      <button class="updateData" type="submit">Update</button>
+      </section>
+    </form>`;
+};
+
+async function getAllApplications(){
+
+  fetch("https://bursarywebapp.azurewebsites.net/api/UniversityApplication/getAllUniversityApplicationsWithName")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(jsonData => {
+      unis = jsonData
+      tableMaker(jsonData);
+      statusColorCoder();
+      for (const b of viewUniversityButtons)
+      b.addEventListener("click", redirectToUniInfo);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+
+getAllApplications()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
