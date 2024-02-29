5d910bc48e1f7788901da3cbdbe4f0edb3476faf
kylebmoodley <kylebmoodley@gmail.com>
2024-02-29 15:41:06 +0200

    BBD landing page has real values from DB

--- Scripts/Fund.js
+++ Scripts/Fund.js
@@ -1,6 +1,19 @@
 const current = document.getElementById("Current");
-const total = document.getElementById("Total")
+const total = document.getElementById("Total");
 
-function fundFinder(){
-    
-}
+async function fundFinder(){
+    try {
+        const response = await fetch("https://bursarywebapp.azurewebsites.net/api/BbdSpendings/GetCurrentBudget");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const responseData = await response.json();
+        current.innerText = `Current Amount: R${responseData["remaining"]}`;
+        total.innerText = `Total Allocation: R${responseData["budget"]}`;
+        console.log('Response:', responseData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+};
+
+fundFinder();
