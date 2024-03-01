069efa91c150796e11a0e5be8dff86abb940cf98
Thando <noluthandoh14@gmail.com>
2024-03-01 08:42:48 +0200

    Added icon on the HTML files

--- Scripts/addStudent.js
+++ Scripts/addStudent.js
@@ -3,5 +3,5 @@ const addStudentButton = document.querySelector('.add');
 
 addStudentButton.addEventListener('click', function() {
 
- window.location.href = '/hod/studentapplicationindex.html';
+ window.location.href = '/hod/studentapplicationindex';
 });
